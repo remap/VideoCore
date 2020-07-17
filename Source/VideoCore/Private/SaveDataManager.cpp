d4a834448a15720023dcac62b5a46b8a7d47f14a
bsubbaraman <bms12014@mymail.pomona.edu>
2020-07-17 13:40:30 -0700

    playback/record included

--- Source/VideoCore/Private/SaveDataManager.cpp
+++ Source/VideoCore/Private/SaveDataManager.cpp
@@ -6,40 +6,40 @@
 // Sets default values
 ASaveDataManager::ASaveDataManager()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+     // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
 }
 
 // Called when the game starts or when spawned
 void ASaveDataManager::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+    
 }
 
 // Called every frame
 void ASaveDataManager::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 
 }
 
 bool ASaveDataManager::SaveData()
 {
-//    SaveDir = SaveDir.TrimStartAndEnd();
-//    if (!SaveDir.EndsWith(FString("/")) or !SaveDir.EndsWith(FString("\\")))
-//    {
-//        SaveDir = SaveDir.Append(FString("/"));
-//    }
-//    FString FilePath = SaveDir  + FString("Python/RecordedTransforms.txt");
-//    FString FileContent = TEXT("");
-//    for (auto Item : recordedTransforms)
-//    {
-//        FileContent += Item.ToString() + TEXT("\n");
-//    }
-//    FFileHelper::SaveStringToFile(FileContent, *FilePath, FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get(), EFileWrite::FILEWRITE_Append);
-//    //now timesteps
+    SaveDir = SaveDir.TrimStartAndEnd();
+    if ((!SaveDir.EndsWith(FString("/"))) || (!SaveDir.EndsWith(FString("\\"))))
+    {
+        SaveDir = SaveDir.Append(FString("/"));
+    }
+    FString FilePath = SaveDir  + FString("Python/RecordedTransforms.txt");
+    FString FileContent = TEXT("");
+    for (auto Item : recordedTransforms)
+    {
+        FileContent += Item.ToString() + TEXT("\n");
+    }
+    FFileHelper::SaveStringToFile(FileContent, *FilePath, FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get(), EFileWrite::FILEWRITE_Append);
+    //now timesteps
 ////    FilePath = TEXT("/Users/remap/Desktop/SaveTimes.txt");
 ////    FileContent = TEXT("");
 ////    for (auto Item : cropPoints)
@@ -53,22 +53,22 @@ bool ASaveDataManager::SaveData()
 
 bool ASaveDataManager::SavePoints()
 {
-//    SaveDir = SaveDir.TrimStartAndEnd();
-//    // do some sanity checks here
-//    if (!SaveDir.EndsWith(FString("/")) or !SaveDir.EndsWith(FString("\\")))
-//    {
-//        SaveDir = SaveDir.Append(FString("/"));
-//    }
-//    FString FilePath = SaveDir;
-//    FString FileContent = TEXT("");
-//
-//    for (auto& Item : cropPoints)
-//    {
-//        FilePath = SaveDir + FString("Python/Crop_") + FString::FromInt(Item.Key) + FString(".txt");
-//        FVector4 points = Item.Value;
-//        FileContent += FString::SanitizeFloat(points.X) + FString(" ") + FString::SanitizeFloat(points.Y) + FString(" ") + FString::SanitizeFloat(points.Z) + FString(" ") + FString::SanitizeFloat(points.W);
-//    }
-//    FFileHelper::SaveStringToFile(FileContent, *FilePath, FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get());
-//
+    SaveDir = SaveDir.TrimStartAndEnd();
+    // do some sanity checks here
+    if ((!SaveDir.EndsWith(FString("/"))) || (!SaveDir.EndsWith(FString("\\"))))
+    {
+        SaveDir = SaveDir.Append(FString("/"));
+    }
+    FString FilePath = SaveDir;
+    FString FileContent = TEXT("");
+
+    for (auto& Item : cropPoints)
+    {
+        FilePath = SaveDir + FString("Python/Crop_") + FString::FromInt(Item.Key) + FString(".txt");
+        FVector4 points = Item.Value;
+        FileContent += FString::SanitizeFloat(points.X) + FString(" ") + FString::SanitizeFloat(points.Y) + FString(" ") + FString::SanitizeFloat(points.Z) + FString(" ") + FString::SanitizeFloat(points.W);
+    }
+    FFileHelper::SaveStringToFile(FileContent, *FilePath, FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get());
+
     return true;
 }
